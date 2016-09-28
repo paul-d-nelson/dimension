2c0a7d25e694bd16d6037515e276c687a07a2fd8
Paul Nelson <pauljimmyclarence@gmail.com>
2016-09-28 15:53:41 -0700

    Initial commit
    Bare bones download of _s

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Dimension
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dimension' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'dimension' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'dimension' ), 'dimension', '<a href="http://paulnelsondev.com" rel="designer">Paul Nelson</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
