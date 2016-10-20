5b51eafebc37d2b141187bc4563ac783be0dd24c
Paul Nelson <pauljimmyclarence@gmail.com>
2016-10-20 11:12:43 -0700

    Fixed featured header hover

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<div class="featured-content">
+<div class="featured">
 
 	<?php
 		$featured_posts = dimension_get_featured_posts();
@@ -14,19 +14,17 @@
 			setup_postdata( $post );
 	?>
 
-		<a href="<?php the_permalink(); ?>" rel="bookmark">
+		<a href="<?php the_permalink(); ?>" rel="bookmark"></a>
+
 		<?php if ( has_post_thumbnail() ) : ?>
-			<!-- <figure class="featured-thumb">
-				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-			</figure> -->
 			<div class="featured-thumb" style="background: url('<?php the_post_thumbnail_url(); ?>') no-repeat center center;background-size: cover;"></div>
 		<?php endif; ?>
-			<header class="featured-header">
-				<h2>Featured</h2>
-				<h1><?php the_title(); ?></h1>
-				<?php the_excerpt(); ?>
-			</header>
-		</a>
+
+		<header class="featured-header">
+			<h2>Featured</h2>
+			<h1><?php the_title(); ?></h1>
+			<?php the_excerpt(); ?>
+		</header>
 
 	<?php
 		endforeach;
